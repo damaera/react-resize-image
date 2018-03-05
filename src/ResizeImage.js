797513425f6c55f00c53a85c0b50267834690329
Damaera <damaera@live.com>
2018-03-05 20:16:10 +0700

    getRszIo in app

--- src/ResizeImage.js
+++ src/ResizeImage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import getRSZioUrl from './getRSZioUrl'
+
 const ResizeImage = (props) => {
   const { src, options, style, alt, resizeActive } = props
 
